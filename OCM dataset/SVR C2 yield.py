593ac2d738532dec4eee048f5c915a786fe4f3b2
tvanhaute <85747640+tvanhaute@users.noreply.github.com>
2021-06-11 17:05:25 +0200

    Update and rename SVR yields.py to SVR C2 yield.py

--- OCM dataset/SVR C2 yield.py
+++ OCM dataset/SVR C2 yield.py
@@ -4,22 +4,21 @@ import sklearn
 from sklearn import preprocessing
 from sklearn.inspection import permutation_importance
 from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.svm import SVR
 
 # read data
-data = pd.read_csv("../OCM-NguyenEtAl.csv")
+data = pd.read_csv("OCM-NguyenEtAl.csv")
 
-# convert M1, M2 and M3 into numeric values
+# convert M1, M2 and M3 into numeric values, superfluous and only included because of me working with multiple algorithms
 le = preprocessing.LabelEncoder()
 data["M1"] = le.fit_transform(data["M1"])
 data["M2"] = le.fit_transform(data["M2"])
 data["M3"] = le.fit_transform(data["M3"])
 
+# define labels, features and dropped features
 predict = ["CH4_conv", "C2y", "C2H6y", "C2H4y", "COy", "CO2y"]
-# features=["Name","M1","M1_atom_number","M2","M2_atom_number","M3","M3_atom_number","Support","Support_ID","M2_mol","M3_mol","M1_mol%","M2_mol%","M3_mol%","Temp","Total_flow","Ar_flow","CH4_flow","O2_flow","CT","CH4/O2"]
 categorical_columns = ["M1", "M2", "M3", "Support_ID"]
 numerical_columns = ["M2_mol", "M3_mol", "Temp", "Ar_flow", "CH4_flow", "O2_flow", "M1_mol", "CT"]
 features = categorical_columns + numerical_columns
@@ -27,19 +26,20 @@ dropped = ["CH4_conv", "C2y", "C2H6y", "C2H4y", "COy", "CO2y", "C2s", "C2H6s", "
            "M1_atom_number", "M2_atom_number", "M3_atom_number", "Support", "CH4/O2", "Total_flow", "M1_mol%",
            "M2_mol%", "M3_mol%"]
 
-data["M2_mol%"] = data["M2_mol%"] + 1e-7
+# calculate M1 mol as it isn't included in the original dataset
+data["M2_mol%"] = data["M2_mol%"] + 1e-7 # to avoid divide by zero errors
 data["M1_mol"] = data["M1_mol%"] * (data["M2_mol"] + data["M3_mol"]) / (data["M2_mol%"] + data["M3_mol%"])
 
+# define label
 voorspel = predict[1]  # choose yield to predict
-number_of_splits = 10
-C_values = [0.1, 1]
-discard_fraction = 0
 
+# define arrays for label and features, standardize label
 X = data[features]
 y = np.array(data[voorspel])
 y = preprocessing.StandardScaler().fit_transform(y.reshape(-1, 1))
 y = np.ravel(y.reshape(-1, 1))
 
+# feature preprocessing
 categorical_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
 numerical_pipe = Pipeline([
     ('imputer', preprocessing.StandardScaler())
@@ -49,22 +49,21 @@ preprocessing = ColumnTransformer(
     [('cat', categorical_encoder, categorical_columns),
      ('num', numerical_pipe, numerical_columns)])
 
+# define dataframe to store feature importances in
 columns = [feature for feature in features]
-
 columns.append("Train score")
 columns.append("Test score")
-
 df1 = pd.DataFrame(columns=columns)
 
+# loop that performs multiple dataset splits and model constructions, chooses best model and saves feature importances for that model
+number_of_splits = 10
+C_values = [0.1, 1]
 for l in range(number_of_splits):
     # split data into test and validation + training data
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
     x_train, x_validate, y_train, y_validate = sklearn.model_selection.train_test_split(x_train, y_train,
                                                                                         test_size=0.11)
 
-    if discard_fraction != 0:
-        x_train, x_discard, y_train, y_discard = sklearn.model_selection.train_test_split(x_train, y_train,
-                                                                                          test_size=discard_fraction)
     Validation_score = 0
     Train_score = 0
     Test_score = 0
@@ -95,4 +94,5 @@ for l in range(number_of_splits):
 
     df1.loc[l] = df_entry1
 
+# print feature importances to a csv file           
 df1.to_csv("Results SVR yields Nguyen et al.csv")
